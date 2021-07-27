b4f10345c041631f85033e5ffbe1a28055f4908e
Scarjit <ferdinand@linnenberg.dev>
2021-07-28 00:21:36 +0200

    feat: so much sql

--- src/generator.rs
+++ src/generator.rs
@@ -1,5 +1,10 @@
-use rand_distr::{Distribution, Normal, NormalError};
+use rand_distr::{Distribution, Normal};
 use rand::thread_rng;
+use rand::seq::SliceRandom;
+use std::fmt::{Display, Formatter};
+use async_fs::File;
+use futures_lite::AsyncWriteExt;
+use std::path::{Path};
 
 #[derive(Debug, Clone)]
 pub struct Node {
@@ -13,47 +18,77 @@ pub struct Graph {
     pub edges: Vec<(usize, usize)>
 }
 
-pub async fn generate_graph(initial_childen: usize, std_dev: f64, std_increase_factor: f64, max_depth: usize) -> Graph {
-    let mut graph: Graph = Graph{
-        nodes: vec![],
-        edges: vec![]
-    };
-
-    let mut nodeid = 0;
-    for i in 0..initial_childen {
-        graph.nodes.push(Node{std_dev, id: nodeid });
-        nodeid += 1;
+impl Display for Graph{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Nodes: {}\nEdges: {}", self.nodes.len(), self.edges.len())
     }
+}
 
-    let mut current_std_dev = std_dev;
-    let mut depth = 0;
-    while current_std_dev > 0f64 && depth < max_depth {
-        let new_std_dev = current_std_dev + std_increase_factor;
-        for node in graph.nodes.clone() {
-            if node.std_dev == current_std_dev {
-                let childs = generate_childen(node.std_dev).await;
-                for i in 0..childs {
-                    graph.nodes.push(Node{std_dev: new_std_dev, id: nodeid });
-                    graph.edges.push((node.id, nodeid));
-                    nodeid += 1;
+impl Graph{
+    pub async fn generate(initial_childen: usize, std_dev: f64, std_increase_factor: f64, max_depth: usize, nodeid: &mut usize) -> Self{
+        let mut graph: Graph = Graph{
+            nodes: vec![],
+            edges: vec![]
+        };
+
+        for _ in 0..initial_childen {
+            graph.nodes.push(Node{std_dev, id: *nodeid });
+            *nodeid += 1;
+        }
+
+        let mut current_std_dev = std_dev;
+        let mut depth = 0;
+        while current_std_dev > 0f64 && depth < max_depth {
+            let new_std_dev = current_std_dev + std_increase_factor;
+            for node in graph.nodes.clone() {
+                if node.std_dev == current_std_dev {
+                    let childs = Self::generate_childen(node.std_dev).await;
+                    for _ in 0..childs {
+                        graph.nodes.push(Node{std_dev: new_std_dev, id: *nodeid });
+                        graph.edges.push( (node.id, *nodeid));
+                        *nodeid += 1;
+                    }
                 }
             }
+            current_std_dev = new_std_dev;
+            depth += 1;
         }
-        current_std_dev = new_std_dev;
-        depth += 1;
+        return graph
     }
-    return graph
+
+    async fn generate_childen(std_dev: f64) -> usize {
+        let mut rng = thread_rng();
+        let normal = Normal::new(0f64, std_dev).unwrap();
+        normal.sample(&mut rng).abs().round() as usize
+    }
+
+
+    pub async fn mutator(self: &mut Graph, mutation_percent: usize){
+        let max_nodes = &self.nodes.len();
+        let n_picked_nodes = (*max_nodes as f64 * (mutation_percent as f64/100f64)).round();
+        let mut rng = thread_rng();
+        for choosen_node in self.nodes.choose_multiple(&mut rng, n_picked_nodes as usize) {
+            let other_node = self.nodes.choose(&mut rng).unwrap();
+            self.edges.push((choosen_node.id, other_node.id))
+        }
+    }
+
+    pub async fn dump_sql_simple(self: Graph, filename: &Path){
+        let mut f = File::create(filename).await.unwrap();
+
+        let mut sql = String::from("INSERT INTO simple_table (parent, child) VALUES\n");
+
+        for edge in self.edges {
+            sql += &format!("\t({}, {}),\n", edge.0, edge.1);
+        }
+        sql.remove(sql.len()-1);
+        sql.remove(sql.len()-1);
+        sql += ";";
+
+        f.write_all(sql.as_bytes()).await.unwrap();
+    }
+
 }
 
 
-pub async fn generate_childen(std_dev: f64) -> usize {
-    let mut rng = thread_rng();
-    let normal = Normal::new(0f64, std_dev).unwrap();
-    normal.sample(&mut rng).abs().round() as usize
-}
 
-pub async fn graph_mutator(graph: &mut Graph, mutation_percent: usize){
-    let max_nodes = &graph.nodes.len();
-    let n_picked_nodes = max_nodes as f64 * (mutation_percent as f64/100f64);
-
-}
--- src/lib.rs
+++ src/lib.rs
@@ -3,23 +3,68 @@ pub mod generator;
 
 #[cfg(test)]
 mod tests {
+    use rand::{thread_rng, Rng};
+    use std::path::Path;
+    use std::fs;
+    use tokio_postgres::NoTls;
+
     #[tokio::test]
     async fn generate_data() {
-        let graph_decrease = crate::generator::generate_graph(
-            5,
-            10f64,
-            -1f64,
-            10
-        ).await;
+        let mut random = thread_rng();
+        let mut all_nodes = 0;
+        let mut all_edges = 0;
+        let mut initial_id = 0;
+        for i in 0..5 {
+            let initial_children = random.gen_range(3..=5);
+            let std_dev = random.gen_range(7f64..=10f64);
+            let std_increase_factor = random.gen_range(-1f64..1f64);
+            let mutator_percentage = random.gen_range(0..=10);
+            println!("Generating graph with");
+            println!("\tInitial children:\t\t{}", initial_children);
+            println!("\tStd Dev:\t\t\t\t{}", std_dev);
+            println!("\tStd Increase factor:\t{}", std_increase_factor);
+            println!("\tMutator percentage:\t{}", std_increase_factor);
 
-        let graph_increase = crate::generator::generate_graph(
-            5,
-            10f64,
-            1f64,
-            7
-        ).await;
+            let mut graph = crate::generator::Graph::generate(
+                initial_children,
+                std_dev,
+                std_increase_factor,
+                6,
+                &mut initial_id
+            ).await;
+            graph.mutator(mutator_percentage).await;
+            println!("\tNodes:\t\t\t\t\t{}", graph.nodes.len());
+            println!("\tEdges:\t\t\t\t\t{}", graph.edges.len());
+            all_nodes += graph.nodes.len();
+            all_edges += graph.edges.len();
+            graph.dump_sql_simple(Path::new(&format!("sql/simple/{}.sql", i))).await;
+            println!("\tFinished graph");
 
-        println!("{:?}", graph_decrease.nodes.len());
-        println!("{:?}", graph_increase.nodes.len());
+        }
+
+        println!("---");
+        println!("nNodes:\t{}", all_nodes);
+        println!("nEdges:\t{}", all_edges);
+    }
+
+
+    #[tokio::test]
+    async fn load_simple_data() {
+        let (client, connection) =
+            tokio_postgres::connect("host=localhost user=postgres password=Katzen123 dbname=test", NoTls).await.unwrap();
+        tokio::spawn(async move {
+            if let Err(e) = connection.await {
+                eprintln!("connection error: {}", e);
+            }
+        });
+
+
+        for f in fs::read_dir("sql/simple/").unwrap() {
+            let path = f.unwrap();
+            let sql = async_fs::read_to_string(path.path()).await.unwrap();
+            client.execute(sql.as_str(), &[]).await.unwrap();
+            println!("Executed {}", path.file_name().to_str().unwrap())
+        }
+
     }
 }
